49c1173d22a634796789d8f16508c05357258d38
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-07-13 21:22:56 +0700

    has been pwneed

--- oddie.py
+++ oddie.py
@@ -6,3 +6,11 @@ print(f'''
  `---'  `---'  `---' `--' `----'
 distinguish that the number is odd or even.
 ''')
+
+a = input("[+] masukan nomer: ")
+aInt = int(float(a)) # float di dalam int tujuannya adalah untuk membulatkan input kebawah, dengan prinsip int yaitu bilangan bulat Jadi programnya akan membulatkan ke bawah.
+
+if aInt % 2 == 0:
+    print(f"angka {a} adalah angka genap")
+else:
+    print(f"angak {a} adalah angka ganjil")
